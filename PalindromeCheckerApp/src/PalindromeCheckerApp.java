afc31d1ad7f78262da056bc402fb293c919f6a73
VijayAnand33 <vijayanand8208@gmail.com>
2026-03-10 14:38:43 +0530

    UC4: Character Array Based Palindrome Check

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,46 +1,27 @@
-/*
-* =========================================
-* MAIN CLASS UseCase1Palindrome App
-* =========================================
-* Use Case 1: Application Entry & Welcome Message
-* Description:
-* This class represents the entry point of the
-* Palindrome Checker Management System.
-* At this stage, the application:
-* Starts execution from the main() method
-* Displays a welcome message
-* Shows application version
-* No palindrome logic is implemented yet.
-* The goal is to establish a clear startup flow.
-  @author Vijay Anand D S
-  @version 1.0
-
-public class UseCase1PalindromeCheckerApp
-/**
-Application entry point.
-This is the first method executed by the JVM
-when the program starts.
-* @param args Command-Line arguments
-public static void main(String[] args) {...}
- */
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String original = "level";
-        String reversed = "";
+        String word = "radar";
+        char[] arr = word.toCharArray();
 
-        // Reverse the string using loop
-        for(int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
+        int start = 0;
+        int end = arr.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (arr[start] != arr[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
 
-        // Compare original and reversed string
-        if(original.equals(reversed)) {
-            System.out.println(original + " is a Palindrome");
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
         } else {
-            System.out.println(original + " is Not a Palindrome");
+            System.out.println(word + " is Not a Palindrome");
         }
     }
 }
