a4c287edd618b25b7d96fe38b23f24c7e477c184
VijayAnand33 <vijayanand8208@gmail.com>
2026-03-10 15:35:43 +0530

    UC6: Deque-Based Optimized Palindrome Checker

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,25 +1,49 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String word = "radar";
-        char[] arr = word.toCharArray();
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-        int start = 0;
-        int end = arr.length - 1;
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Create Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Create Stack (LIFO)
+        Stack<Character> stack = new Stack<>();
+
+        // Create Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // Enqueue and Push characters
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
+        }
+
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (arr[start] != arr[end]) {
+        // Compare dequeue (queue) with pop (stack)
+        while (!stack.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
+        // Print result
         if (isPalindrome) {
-            System.out.println(word + " is a Palindrome");
+            System.out.println("The given string is a Palindrome.");
         } else {
-            System.out.println(word + " is Not a Palindrome");
+            System.out.println("The given string is NOT a Palindrome.");
         }
+
+        sc.close();
     }
 }
